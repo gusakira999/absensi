a056e5ede0f953d9d84101c7e0fad6bc89d71619
dayat <hidayatachmad316@gmail.com>
2026-06-17 14:57:03 +0800

    update

--- resources/views/layouts/app/sidebar.blade.php
+++ resources/views/layouts/app/sidebar.blade.php
@@ -1,8 +1,5 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
-    <head>
-        @include('partials.head')
-    </head>
     <body class="min-h-screen bg-white dark:bg-zinc-800">
         <flux:sidebar sticky collapsible="mobile" class="border-e border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
             <flux:sidebar.header>
@@ -15,12 +12,6 @@
                     <flux:sidebar.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>
                         {{ __('Dashboard') }}
                     </flux:sidebar.item>
-<<<<<<< HEAD
-                    <flux:sidebar.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>
-                        {{ __('asu') }}
-                    </flux:sidebar.item>
-=======
->>>>>>> 67a71439af8aa1816d22c6ad3c801d3367f117bf
                 </flux:sidebar.group>
             </flux:sidebar.nav>
 
