a65b67eaa700ac9886ba93f64bada5b3cddcfb42
dayat <hidayatachmad316@gmail.com>
2026-06-27 15:46:37 +0800

    update models

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Attendance extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'course_id',
+        'attendance_date',
+        'status',
+        'check_in_time',
+    ];
 }
--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Schedule extends Model
 {
-    //
+    protected $fillable = [
+        'course_id',
+        'day',
+        'start_time',
+        'end_time',
+        'room'
+    ];
 }
